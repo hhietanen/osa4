4fbdaa217d42338d812e2aa71926b244f81c69b0
Herkko <herkko@gmail.com>
2018-04-15 14:36:57 +0300

    Nyt tehtävät 4.8 - 4.11 tehty.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,25 +3,43 @@ const Blog = require('../models/blog')
 
 
 blogRouter.get('/', async (request, response) => {
-  // Blog
-  //   .find({})
-  //   .then(blogs => {
-  //     response.json(blogs)
-  //   })
+  try{
     const blogs = await Blog.find({})
-//    console.log(blogs)
     response.json(blogs)
+  } catch (exception) {
+    console.log(exception)
+    response.status(400).send({ error: 'error in retrieving blogs' })
+  }
 })
 
-blogRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogRouter.post('/', async (request, response) => {
+  try {
+    const body = request.body
+    if (body.title === undefined){
+      return response.status(400).json({error : 'title missing'})
+    }
+
+    // if (body.likes === undefined || body.likes === null  ){
+    //    body.likes=0
+    // }
+
+    const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes ? body.likes : 0
+
+
+    })
   console.log(blog)
 
-  blog
+  const savedBlog = await blog
     .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+      response.status(201).json(blog)
+    } catch (exception) {
+      console.log(exception)
+      response.status(500).json({error : 'things went wrong'})
+    }
 })
 
 module.exports = blogRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -36,7 +36,6 @@ test('all blogs are returned', async () => {
     .get('/api/blogs')
 
   expect(response.body.length).toBe(initialBlogs.length)
-//  expect(response.body.length).toBe(0)
 })
 
 test('blogs are returned as json', async () => {
@@ -52,9 +51,85 @@ test('the first blog to be about React patterns', async () => {
     .get('/api/blogs')
 
   const contents = await response.body.map(r => r.title)
-  expect(contents).toContain('React patterns')
-  
+  expect(contents).toContain('React patterns')  
 })
+
+test('a valid blog can be added', async () => {
+	const newBlog =   {
+    title: "Dingo palaa kotiin",
+    author: "Edgar Aller",
+    url: "http://www.slahdot.com",
+    likes: 6
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api
+      .get('/api/blogs')
+
+    const contents = response.body.map(r => r.title)
+
+	expect(response.body.length).toBe(initialBlogs.length +1)
+	expect(contents).toContain('Dingo palaa kotiin')
+})
+
+test('if there are no likes, likes are set to zero', async () => {
+	const zeroBlog =   {
+    title: "Kukaan ei tykkää tästä",
+    author: "Matti Nykänen",
+    url: "http://www.sseika.com",
+    likes: ""
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(zeroBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api
+      .get('/api/blogs')
+
+    const contents = response.body.map(r => r.likes)
+    const likes = response.body[initialBlogs.length +1].likes
+
+	expect(response.body.length).toBe(initialBlogs.length +2)
+	expect(likes).toBe(0)  
+})
+
+
+
+test('if there are no title and url, 400 bad request is returned', async () => {
+	const errorBlog =   {
+    author: "Matti Mogaaja",
+    likes: "99"
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(errorBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api
+      .get('/api/blogs')
+
+    const contents = response.body.map(r => r.author)
+	expect(contents).not.toContain('Matti Mogaaja')
+
+})
+
+// Tee testit blogin lisäämiselle, eli osoitteeseen 
+// /api/blogs tapahtuvalle HTTP POST -pyynnölle, joka varmistaa, 
+// että jos uusi blogi ei sisällä kenttiä title ja url, 
+// pyyntöön vastataan statuskoodilla 400 Bad request
+
+// Laajenna toteutusta siten, että testit menevät läpi.
+
 
 afterAll(() => {
   server.close()
